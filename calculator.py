ca1290c1cb817788229586220414c0b9f682303d
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-10 17:35:33 -0700

    added a loop to convert infinate strings to floats

--- calculator.py
+++ calculator.py
@@ -8,30 +8,55 @@ from arithmetic import *
 
 def evaluate_input(calcinput):
 
+
+    
+    arguments = calcinput[1:]
+    array = []
+
+    for argument in arguments:
+        array.append(floats(argument))
+    print(array,"this is working")    
+        
+
+
+    #print("arguments working!", arguments)
+    """evaluates user input and calls appropriate arithmetic function"""
+
     prefix = calcinput[0]
+    
     if prefix == "q":
         return "q"
+    
     if len(calcinput) == 2:
         calcinput1 = float(calcinput[1])  
 
-    elif len(calcinput) == 3:    
+    elif len(calcinput) >= 3:    
         calcinput1 = float(calcinput[1])
         calcinput2 = float(calcinput[2])
+            
+
         
     if prefix == '+':
         return add(calcinput1, calcinput2)
+    
     elif prefix == "-":
         return subtract(calcinput1, calcinput2)
+    
     elif prefix == "*":
         return multiply(calcinput1, calcinput2)
+    
     elif prefix == "/":
         return divide(calcinput1, calcinput2)
+    
     elif prefix == "square":
         return square(calcinput1)
+    
     elif prefix == "cube":
         return cube(calcinput1)
+    
     elif prefix == "pow":
         return power(calcinput1, calcinput2)
+    
     elif prefix == "mod":
         return mod(calcinput1, calcinput2)    
 
@@ -55,10 +80,15 @@ def evaluate_input(calcinput):
 while True:
     calcinput = input()
     calcinput = calcinput.split()
+    #print(*calcinput)
+   
     output = evaluate_input(calcinput) 
     if output == "q":
         break
-    print(output)
+    if output == None:
+        print("thats..... not right!")
+    else:        
+        print(output)
 
      #TODO: Need to build out this function 
 
